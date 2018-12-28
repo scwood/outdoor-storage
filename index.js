38bd6ab2380b1cadd613d5964af4624153c8d6e1
Spencer Wood <spencercwood@gmail.com>
2018-12-28 01:54:06 -0500

    Move to client side solution

--- index.js
+++ index.js
@@ -1,88 +1,79 @@
-const csvParse = require('csv-parse/lib/sync');
-const express = require('express');
-const fileUpload = require('express-fileupload');
-const path = require('path');
-
-function checkForFiles(fileNames) {
-  return (req, res, next) => {
-    const { files } = req;
-    if (!files) {
-      reportMissingFiles(res, fileNames);
-      return;
-    }
-    const missing = fileNames.filter(name => !(name in files));
-    if (missing.length > 0) {
-      reportMissingFiles(res, missing);
-      return;
-    }
-    next();
-  }
+function createTransformedFile() {
+  var unitFile = getFileById('unitFile')
+  var customerFile = getFileById('customerFile')
+  var templateFile = getFileById('templateFile')
+  Promise.all([
+    parseFileAsCsv(unitFile),
+    parseFileAsCsv(customerFile),
+    parseFileAsText(templateFile)
+  ])
+    .then(function(parsedFiles) {
+      var units = parsedFiles[0].data
+      var customers = parsedFiles[1].data
+      var template = parsedFiles[2]
+      var unitsByNumber = createHash(units, 'UnitNo')
+      var activeUnits = units.filter(function (unit) {
+        return unit.CustID !== '' && unit.MODate === ''
+      })
+      var activeUnitsByNumber = createHash(activeUnits, 'UnitNo')
+      var customersById = createHash(customers, 'CustID')
+      var unitsToReplace = template.match(/\d\d-...../g)
+      unitsToReplace.forEach(function (number) {
+        var result
+        if (!(number in unitsByNumber)) {
+          // (1) Unit does not exist in database
+          result = 'ERROR'
+        } else if (number in activeUnitsByNumber) {
+          // (2) Unit is being rented by a customer
+          var customerId = activeUnitsByNumber[number].CustID
+          var customer = customersById[customerId]
+          result = customer.CName
+        } else {
+          // (3) Unit is not being rented
+          result = ''
+        }
+        result = result.replace(',', '')
+        template = template.replace(number, result)
+      })
+      download('inventory.csv', template)
+    })
 }
 
-function reportMissingFiles(res, missing) {
-  res.status(400).send({
-    error: `Missing required file(s): ${missing.join(', ')}`
-  });
+function getFileById(id) {
+  return document.getElementById(id).files[0]
 }
 
-function doTransform(req, res) {
-  const { unitFile, customerFile, templateFile } = req.files;
-  const units = parseCsv(unitFile);
-  const unitsByNumber = createHash(units, 'UnitNo');
-  const activeUnits = units.filter(u => u.CustID !== '' && u.MODate === '');
-  const activeUnitsByNumber = createHash(activeUnits, 'UnitNo');
-  const customersById = createHash(parseCsv(customerFile), 'CustID');
+function parseFileAsCsv(file) {
+  return new Promise(function (resolve) {
+    Papa.parse(file, {complete: resolve, header: true})
+  })
+}
 
-  let template = templateFile.data.toString();
-  const unitsToReplace = template.match(/\d\d-...../g);
-
-  unitsToReplace.forEach(number => {
-    let result;
-    if (!(number in unitsByNumber)) {
-      // (1) Unit does not exist in database
-      result = 'ERROR';
-    } else if (number in activeUnitsByNumber) {
-      // (2) Unit is being rented by a customer
-      const customerId = activeUnitsByNumber[number].CustID;
-      const customer = customersById[customerId];
-      result = customer.CName;
-    } else {
-      // (3) Unit is not being rented
-      result = '';
+function parseFileAsText(file) {
+  return new Promise(function (resolve) {
+    var fileReader = new FileReader()
+    fileReader.onload = function () {
+      resolve(fileReader.result)
     }
-    result = result.replace(',', '');
-    template = template.replace(number, result);
-  });
-
-  res.setHeader('Content-type', 'application/octet-stream');
-  res.setHeader('Content-disposition', 'attachment; filename=inventory.csv');
-  res.send(template);
+    fileReader.readAsText(file)
+  })
 }
 
 function createHash(arr, key) {
-  return arr.reduce((acc, curr) => {
-    acc[curr[key]] = curr;
-    return acc;
-  }, {});
+  return arr.reduce(function (acc, curr) {
+    acc[curr[key]] = curr
+    return acc
+  }, {})
 }
 
-function parseCsv(file) {
-  return csvParse(file.data.toString(), { columns: true });
+function download(filename, text) {
+  var element = document.createElement('a')
+  element.setAttribute(
+    'href', 'data:text/plaincharset=utf-8,' + encodeURIComponent(text)
+  )
+  element.setAttribute('download', filename)
+  element.style.display = 'none'
+  document.body.appendChild(element)
+  element.click()
+  document.body.removeChild(element)
 }
-
-const app = new express();
-const publicPath = path.resolve(__dirname, 'public');
-
-app.use(express.static(publicPath));
-app.use(fileUpload());
-
-app.post(
-  '/transform',
-  checkForFiles(['unitFile', 'customerFile', 'templateFile']),
-  doTransform
-);
-
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`listening on port ${port}`);
-});
